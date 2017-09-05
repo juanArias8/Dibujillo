d6c8399d245aee81c89044bb015a10da6cc60437
juasda96 <juasda96@gmail.com>
2017-09-04 22:16:12 -0500

    version 1.0.2 --- se mejora el landing, se agregan validaciones

--- web/Js/main.js
+++ web/Js/main.js
@@ -3,29 +3,50 @@ $( document ).ready(function() {
 	var mainContent = $("#mainContent");
 	var alertWin = $("#alertWin");
 	var alertMain = $("#alertMain");
+	var slideTog = $(".slideTog");
 
 	landing.show(0);
 	mainContent.hide(0);
 	alertWin.hide(0);
 	alertMain.hide(0);
+	slideTog.slideToggle(2000);
 });
 
 landing = $("#landing");
 mainContent = $("#mainContent");
 alertWin = $("#alertWin");
 alertMain = $("#alertMain");
+slideText = $(".slideText");
+slideTog = $(".slideTog");
 var btnIngresar = $("#btnIngresar");
 var nombre = $("#nombre");
 var exampleModal = $("#exampleModal");
 
 var nombres = [];
 
+slideText
+	.on("mouseover", function(){
+		slideTog.slideDown("slow");
+		slideText.css({
+			"background-color": "rgba(0,0,0,1)",
+		    "padding": "3%",
+		    "border-radius": "20px",
+		});		
+	})
+	.on("mouseleave", function(){
+		slideTog.slideUp("slow");
+		slideText.css({
+			"background-color": "rgba(0,0,0,0.5)",
+		    "padding": "3%",
+		    "border-radius": "15px"
+	    });	    
+	});
+
 btnIngresar.on("click", function(){
 	console.log(landing);
 	nombreVal = nombre.val();
 	if(nombreVal != "" && typeof nombreVal != "undefined"){
 		nombres.push(nombreVal);
-		console.log(nombres);
 		nombre.val("");
 		exampleModal.modal("hide");
 		landing.fadeOut("slow");
